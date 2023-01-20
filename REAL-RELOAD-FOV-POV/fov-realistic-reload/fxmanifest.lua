78a4ae918a78ef882564395d413a22ff532fb7b4
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-01-20 20:32:25 -0300

    Add files via upload

---
+++ REAL-RELOAD-FOV-POV/fov-realistic-reload/fxmanifest.lua
@@ -0,0 +1,13 @@
+--ALL EDITS MADE BY ME https://github.com/dsvipeer ;)
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+files {
+  'metas/weapons/*.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'metas/weapons/*.meta'
---
+++ REAL-RELOAD-FOV-POV/weaponsMETA/fxmanifest.lua
@@ -0,0 +1,14 @@
+--ALL EDITS MADE BY ME https://github.com/dsvipeer ;)
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+files {
+  'metas/weapons/weapons.meta',
+  'metas/weapons/weaponanimations.meta'
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'metas/weapons/*.meta'
