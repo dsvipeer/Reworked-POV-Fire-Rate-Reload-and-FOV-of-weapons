a651656d80efe1249299fe07d7a8d0a246859bf7
dsvipeer <112516086+dsvipeer@users.noreply.github.com>
2023-07-13 19:41:24 -0300

    Add files via upload

---
+++ fov-reloadrealista/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+files {
+  'metas/weapons/*.meta',
+}
+
+-- Player weapon meta files.
+data_file 'WEAPONINFO_FILE_PATCH' 'metas/weapons/*.meta'
---
+++ weaponsMETA/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+files {
+  'metas/weapons/weapons.meta',
+  'metas/weapons/weaponanimations.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'metas/weapons/*.meta'
